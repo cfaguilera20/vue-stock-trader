321fe1f050c728cb98920d98def697073f4f3cbe
Carlos Aguilera <a.carlosing@gmail.com>
2018-12-29 12:14:42 -0600

    Creating Stocks component.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
     <router-link to="/" class="navbar-brand">Stock Trader</router-link>
     <button
       class="navbar-toggler"
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>The Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card border-success mb-4">
+      <div class="card-header">
+        NAME
+        <small>(Price: PRICE)</small>
+      </div>
+      <div class="card-body">
+        <!-- <h5 class="card-title">Dark card title</h5> -->
+        <!-- <p class="card-text">TEXT.</p> -->
+        <div class="float-left">
+          <input type="number" class="form-control" placeholder="Quantity">
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
