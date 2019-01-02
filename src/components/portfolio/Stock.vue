2491a3e265c21fdccd3cf07188b470fb3ddde1f3
Carlos Aguilera <a.carlosing@gmail.com>
2019-01-01 21:37:10 -0600

    Fix some errors.

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -36,14 +36,15 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["sellStock"]),
+    ...mapActions({ placeSellOrder: "sellStock" }),
     sellStock() {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
-        quantity: quantity
+        quantity: this.quantity
       };
-      this.sellStock();
+      this.placeSellOrder(order);
+      this.quantity = 0;
     }
   }
 };
